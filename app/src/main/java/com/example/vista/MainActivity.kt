e7dd2006bb0565c78f10192f6a0433a048c10e8d
Eduardo Gil <pisangas@gmail.com>
2021-04-02 13:45:13 -0500

    initial

---
+++ app/src/main/java/com/example/vista/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.vista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
